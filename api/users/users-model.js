3baeccc73859744318e906b6415b204ead9c27aa
Savannah <savmaxwell@gmail.com>
2022-03-29 17:45:40 -0600

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -21,7 +21,7 @@ function find() {
    */
   return db('users as u')
     .join('roles as r', 'u.role_id', 'r.role_id')
-    .select('user_id, username, role_name')
+    .select('user_id', 'username', 'role_name')
 }
 
 function findBy(filter) {
